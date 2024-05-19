8eed6c558cebc3d415894e9c87f19dd452946450
Ben Bartle <benjamin.d.bartle@gmail.com>
2024-05-19 08:57:42 -0500

    better sizing of canvas

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 const fs = require('fs');
-const { createCanvas, registerFont } = require('canvas');
+const { createCanvas } = require('canvas');
 const { program } = require('commander');
 
 program
   .version('1.0.0')
   .option('-f, --font <value>', 'Font', 'CopperPlate Gothic')
   .option('-t, --text <value>', 'Text', 'Bluebonnet')
+  .option('-s, --size <value>', 'Size in Pixels', '40')
   .parse(process.argv);
 
-const { font, text } = program.opts();
+const { font, text, size } = program.opts();
 
 console.log(`Writing ${text} using ${font}!`);
 
 // Set canvas dimensions
-const canvasWidth = 400;
-const canvasHeight = 200;
+const canvasWidth = Number(size)*text.length;
+const canvasHeight = Number(size)*2;
 
 // Create a canvas
 const canvas = createCanvas(canvasWidth, canvasHeight);
 const ctx = canvas.getContext('2d');
 
 // Set font properties
-ctx.font = `bold 40px ${font}`;
+ctx.font = `bold ${size}px ${font}`;
 
 // Draw the text
 ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-ctx.fillText(text, canvasWidth / 2, canvasHeight / 2);
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.fillText(text, canvasWidth/2, canvasHeight/2);
 
 // Save the canvas as a PNG file
 const outputPath = './output.png'; // Replace with the desired output path
